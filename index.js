99962b44d95d5d4f54e4c118cfc7fa7fa3c60a03
unknown <your.email@example.com>
2025-10-25 00:00:40 +0530

    chore: Add vercel and cors dependency for deployment

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ const app = express()
 app.use(express.json())
 require("dotenv").config()
 initializeDatabase()
+const cors = require("cors");
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 
 async function createMovie(newMovie){
